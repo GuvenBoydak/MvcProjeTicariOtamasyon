dc02982b5bc2d63b3319940f7be0afc7fb24e30f
güven boydak <gvn.boydak@gmail.com>
2022-05-01 23:01:13 +0300

    GalleryController Eklendi.

---
+++ Project.UI/Controllers/GalleryController.cs
@@ -0,0 +1,28 @@
+﻿using Project.BLL.DesingPatterns.GenericRepository.ConcreteRep;
+using Project.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.UI.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ProductManager _pManager;
+        public GalleryController()
+        {
+            _pManager = new ProductManager();
+        }
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            GalleryVM vM = new GalleryVM()
+            {
+                Products = _pManager.GetActives()
+            };
+            return View(vM);
+        }
+    }
+}
---
+++ Project.UI/ViewModels/GalleryVM.cs
@@ -0,0 +1,13 @@
+﻿using Project.ENTITIES.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.UI.ViewModels
+{
+    public class GalleryVM
+    {
+        public List<Product> Products { get; set; }
+    }
+}
