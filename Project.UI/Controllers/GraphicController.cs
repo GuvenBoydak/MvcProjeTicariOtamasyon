8b7e9c5639bb2ff00abb9d9fc0990c162a35ba6a
güven boydak <gvn.boydak@gmail.com>
2022-05-04 20:43:04 +0300

    GraphicController içerisine Pie, Line, Column Chartlar eklendi. QRController içerisine QR işlemleri ve QR oluşturucu Eklendi.

---
+++ Project.UI/Controllers/GraphicController.cs
@@ -0,0 +1,52 @@
+﻿using Project.BLL.DesingPatterns.GenericRepository.ConcreteRep;
+using Project.ENTITIES.Concrete.Entities;
+using Project.UI.DTOs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Project.UI.Controllers
+{
+    public class GraphicController : Controller
+    {
+        CategoryManager _cManager;
+        ProductManager _pManager;
+        public GraphicController()
+        {
+            _cManager = new CategoryManager();
+            _pManager = new ProductManager();
+        }
+        // GET: Graphic
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult VisualizeProductResult()
+        {
+            return Json(ProductList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<ProductDTO> ProductList()
+        {
+            List<ProductDTO> dTOs = new List<ProductDTO>();
+
+            dTOs = _pManager.GetActives().Select(x => new ProductDTO { Name = x.Name, Stock = x.Stock }).ToList();
+
+            return dTOs;
+        }
+
+        public ActionResult LineGraphic()
+        {
+            return View();
+        }
+
+        public ActionResult ColumnGraphic()
+        {
+            return View();
+        }
+    }
+}
--- Project.UI/DTOs/ProductDTO.cs
+++ Project.UI/DTOs/ProductDTO.cs
@@ -7,6 +7,8 @@ namespace Project.UI.DTOs
 {
     public class ProductDTO
     {
+        public string Name { get; set; }
+        public short Stock { get; set; }
         public string Brand { get; set; }
         public int Count { get; set; }
         public string Category { get; set; }
